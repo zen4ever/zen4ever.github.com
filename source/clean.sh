6fad6a1b2abb305ee51dd642f1ddda978606e43a
Andrii Kurinnyi <andrew@marpasoft.com>
2012-10-05 04:34:43 -0700

    Added CNAME file to support custom domain

--- source/clean.sh
+++ source/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 NAME=`dirname $1`
-find $NAME -maxdepth 1 | grep -v source | grep -vG \\.git | grep -vG $NAME$ | xargs rm -rf {}
+find $NAME -maxdepth 1 | grep -v CNAME | grep -v source | grep -vG \\.git | grep -vG $NAME$ | xargs rm -rf {}
